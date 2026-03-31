142c8e4f4bfd3ad3f9ad2f7b7f2027dea6a08e00
Jar <jarcristhian@gmail.com>
2026-03-30 22:40:09 -0500

    feat: move splot lights

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -4,7 +4,7 @@ import { Stars } from "./Stars";
 import { useEffect } from "react";
 import { i18next } from "@/i18n/config";
 import ShinyText from "@/components/shinyText";
-import { SpotlightParticles } from "./spotlightParticles";
+import { SpotlightParticles } from "./SpotlightParticles";
 
 const containerVariants: Variants = {
   animate: {
